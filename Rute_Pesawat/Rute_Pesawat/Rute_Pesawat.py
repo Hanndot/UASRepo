3323a9f522c20aaa5224681b3041b312468c8860
Farhan Athaullah <athaullahfarhan@gmail.com>
2020-06-18 23:23:51 +0700

    commit 3

--- Rute_Pesawat/Rute_Pesawat/Rute_Pesawat.py
+++ Rute_Pesawat/Rute_Pesawat/Rute_Pesawat.py
@@ -1,26 +1,26 @@
 graph = {'a':{'b':4,'c':3},'b':{'a':4,'e':12,'f':5},'c':{'a':3,'d':7,'e':10},'d':{'c':7,'e':2},'e':{'b':12,'c':10,'d':2,'g':5},'f':{'b':5,'g':16},'g':{'e':5,'f':16}}
  
 def alg(graph,asal,tujuan):
-    shortest_distance = {}
+    jarak_terpendek = {}
     preD = {}
     unseenNodes = graph
     infinity = float('inf')
     path = []
     for node in unseenNodes:
-        shortest_distance[node] = infinity
-    shortest_distance[asal] = 0
+        jarak_terpendek[node] = infinity
+    jarak_terpendek[asal] = 0
  
     while unseenNodes:
         minNode = None
         for node in unseenNodes:
             if minNode is None:
                 minNode = node
-            elif shortest_distance[node] < shortest_distance[minNode]:
+            elif jarak_terpendek[node] < jarak_terpendek[minNode]:
                 minNode = node
  
         for childNode, weight in graph[minNode].items():
-            if weight + shortest_distance[minNode] < shortest_distance[childNode]:
-                shortest_distance[childNode] = weight + shortest_distance[minNode]
+            if weight + jarak_terpendek[minNode] < jarak_terpendek[childNode]:
+                jarak_terpendek[childNode] = weight + jarak_terpendek[minNode]
                 preD[childNode] = minNode
         unseenNodes.pop(minNode)
  
@@ -33,8 +33,8 @@ def alg(graph,asal,tujuan):
             print('Path not reachable')
             break
     path.insert(0,asal)
-    if shortest_distance[tujuan] != infinity:
-        print('Jaraknya ' + str(shortest_distance[tujuan]) + ' m')
+    if jarak_terpendek[tujuan] != infinity:
+        print('Jaraknya ' + str(jarak_terpendek[tujuan]) + ' m')
         print('Jalurnya melewati  ' + str(path))
 
 print('Daftar Wahana:')
